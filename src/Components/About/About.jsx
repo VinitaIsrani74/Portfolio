1fd609eef4d81e6e8e023d7b76f05211b8d29222
Vinita Israni <138797388+VinitaIsrani74@users.noreply.github.com>
2024-05-06 13:27:55 +0530

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,52 +1,30 @@
 import React from "react";
 import "./about.css";
-import Image from "../../assets/about-image.jpg";
-import Image2 from "../../assets/about-img-2.png";
-
+import AboutImage from "../../assets/aimage2.jpg";
 
 const About = () => {
   return (
     <div className="about-container" data-aos="zoom-in">
-      {/* top-section */}
-      <div className="topSection">
-        <div className="topSection-left">
-          <h2 className="about-section-heading">
-            <span> Hi! I'm Vinita Israni-</span>
-            <br />
-            Eager mind, endless possibilities in tech.
-          </h2>
-          <hr />
-          <br />
-          
-        </div>
-        <div className="topSection-right">
-
-
-          <img src={Image} alt="about" className="about-image" />
+      <div className="about-left">
+        <div className="img-bg">
+        <img src={AboutImage} alt="aboutimg" className="about-image" />
         </div>
       </div>
 
-      {/* bottom-section */}
-      <div className="bottomSection">
-        <img src={Image2} alt="about" className="about-img" />
-        <div className="bottomSection-right">
-          <div className="bottomSection-heading">About Me and My Work</div>
-          <p>
-
-          In my project section, you'll see my journey unfold, showcasing my
-            determination to learn and grow. Each project I've undertaken
-            reflects my passion for mastering new technologies and my
-            willingness to push myself further.
-           
-            <br />
-            <br />
-            Technology excites me, and I'm ready to absorb everything I can. If
-            you bring me on board, you'll get my full commitment and effort. I'm
-            eager to prove myself and contribute to the company's success. With
-            me, you'll have someone who's always eager to learn and grow
-            alongside your team.
-          </p>
+      <div className="about-right">
+        <div className="aheading">
+          <h3>Vinita Israni</h3>
+          <h4>Full Stack Developer: Coding fluency, from UI to database.</h4>
         </div>
+        <div className="desc">
+          While my professional background may not include direct experience in
+          MERN stack development, I've actively sought out opportunities to
+          expand my knowledge and skills in this area. Through personal projects, I've gained proficiency in building
+          full-stack applications using MongoDB, Express.js, React.js, and
+          Node.js. I'm eager to leverage these skills in a professional setting
+          and contribute to innovative projects at your company.
+        </div>
+        <div className="desc">I'll give my all if given the chance, ready to dive into every task. With six months of part-time experience as a MERN stack developer, I'm eager to learn and grow. I've tasted the thrill of coding online, and I'm hungry for more challenges. Give me the opportunity, and I'll prove my dedication and passion for this craft.</div>
       </div>
     </div>
   );
